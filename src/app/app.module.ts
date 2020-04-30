92d873ad2b75769493e5238b5f33427187bb089b
sam <samuel.pelsy@outlook.fr>
2020-04-30 17:09:45 +0200

    Ajout de la liste des burgers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ApiModule } from './swagger/api.module';
+import { BurgerComponent } from './burger/burger.component';
+import { BurgerListComponent } from './burger-list/burger-list.component'; 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BurgerComponent,
+    BurgerListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ApiModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/burger-list/burger-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+import { BurgersService } from '../swagger/api/burgers.service';
+import { Burger } from '../swagger/model/models';
+
+@Component({
+  selector: 'app-burger-list',
+  templateUrl: './burger-list.component.html',
+  styleUrls: ['./burger-list.component.css']
+})
+export class BurgerListComponent implements OnInit {
+
+  //Variable dans laquelle tous les burgers iront
+  burgers;
+  //On donne à notre composant l'api burger
+  constructor(private burgersService : BurgersService) { }
+
+  //Lorsque le composant s'init
+  ngOnInit(): void {
+    //On charge tous les burgers
+    this.getBurgers();
+  }
+
+  //Fonction de récupération des burgers
+  getBurgers() : void {
+    this.burgers = this.burgersService.listBurgers()
+      .subscribe(burgers => this.burgers = burgers)
+  }
+
+}
